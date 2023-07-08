65ed189d224b1485690785c6f7816b7f7d929cb3
devinMcArthur <assortedgerm@gmail.com>
2023-07-07 18:54:59 -0600

    Fix rejections from fileStorage functions, remove debug logs

--- server/src/models/JobsiteMonthReport/class/generate.ts
+++ server/src/models/JobsiteMonthReport/class/generate.ts
@@ -13,165 +13,139 @@ import dayjs from "dayjs";
 const expenseInvoiceReports = async (
   jobsiteMonthReport: JobsiteMonthReportDocument
 ) => {
-  try {
-    // Get all expense invoices
-    const jobsite = await jobsiteMonthReport.getJobsite();
-    const expenseInvoices = await jobsite.getExpenseInvoices();
+  // Get all expense invoices
+  const jobsite = await jobsiteMonthReport.getJobsite();
+  const expenseInvoices = await jobsite.getExpenseInvoices();
 
-    const invoices: InvoiceReportClass[] = [];
-    for (let i = 0; i < expenseInvoices.length; i++) {
-      const expenseInvoice = expenseInvoices[i];
+  const invoices: InvoiceReportClass[] = [];
+  for (let i = 0; i < expenseInvoices.length; i++) {
+    const expenseInvoice = expenseInvoices[i];
 
-      if (
-        dayjs(expenseInvoice.date).isSame(
-          dayjs(jobsiteMonthReport.startOfMonth),
-          "month"
-        )
-      ) {
-        const invoice: InvoiceReportClass = {
-          invoice: expenseInvoice._id,
-          value: expenseInvoice.cost,
-          internal: expenseInvoice.internal,
-          accrual: expenseInvoice.accrual,
-        };
+    if (
+      dayjs(expenseInvoice.date).isSame(
+        dayjs(jobsiteMonthReport.startOfMonth),
+        "month"
+      )
+    ) {
+      const invoice: InvoiceReportClass = {
+        invoice: expenseInvoice._id,
+        value: expenseInvoice.cost,
+        internal: expenseInvoice.internal,
+        accrual: expenseInvoice.accrual,
+      };
 
-        invoices.push(invoice);
-      }
+      invoices.push(invoice);
     }
-
-    jobsiteMonthReport.expenseInvoices = invoices;
-
-    return;
-  } catch (error) {
-    console.error("Error in Expense Invoice Reports", error);
   }
+
+  jobsiteMonthReport.expenseInvoices = invoices;
+
+  return;
 };
 
 const revenueInvoiceReports = async (
   jobsiteMonthReport: JobsiteMonthReportDocument
 ) => {
-  try {
-    // Get all revenue invoices
-    const jobsite = await jobsiteMonthReport.getJobsite();
-    const revenueInvoices = await jobsite.getRevenueInvoices();
+  // Get all revenue invoices
+  const jobsite = await jobsiteMonthReport.getJobsite();
+  const revenueInvoices = await jobsite.getRevenueInvoices();
 
-    const invoices: InvoiceReportClass[] = [];
-    for (let i = 0; i < revenueInvoices.length; i++) {
-      const revenueInvoice = revenueInvoices[i];
+  const invoices: InvoiceReportClass[] = [];
+  for (let i = 0; i < revenueInvoices.length; i++) {
+    const revenueInvoice = revenueInvoices[i];
 
-      if (
-        dayjs(revenueInvoice.date).isSame(
-          dayjs(jobsiteMonthReport.startOfMonth),
-          "month"
-        )
-      ) {
-        const invoice: InvoiceReportClass = {
-          invoice: revenueInvoice._id,
-          value: revenueInvoice.cost,
-          internal: revenueInvoice.internal,
-          accrual: revenueInvoice.accrual,
-        };
+    if (
+      dayjs(revenueInvoice.date).isSame(
+        dayjs(jobsiteMonthReport.startOfMonth),
+        "month"
+      )
+    ) {
+      const invoice: InvoiceReportClass = {
+        invoice: revenueInvoice._id,
+        value: revenueInvoice.cost,
+        internal: revenueInvoice.internal,
+        accrual: revenueInvoice.accrual,
+      };
 
-        invoices.push(invoice);
-      }
+      invoices.push(invoice);
     }
-
-    jobsiteMonthReport.revenueInvoices = invoices;
-
-    return;
-  } catch (error) {
-    console.error("Error in Revenue Invoice Reports", error);
   }
+
+  jobsiteMonthReport.revenueInvoices = invoices;
+
+  return;
 };
 
 const summary = async (jobsiteMonthReport: JobsiteMonthReportDocument) => {
-  try {
-    let externalExpenseInvoiceValue = 0,
-      internalExpenseInvoiceValue = 0,
-      accrualExpenseInvoiceValue = 0;
-    for (let i = 0; i < jobsiteMonthReport.expenseInvoices.length; i++) {
-      if (jobsiteMonthReport.expenseInvoices[i].accrual) {
-        accrualExpenseInvoiceValue +=
-          jobsiteMonthReport.expenseInvoices[i].value;
-      } else if (jobsiteMonthReport.expenseInvoices[i].internal) {
-        internalExpenseInvoiceValue +=
-          jobsiteMonthReport.expenseInvoices[i].value;
-      } else {
-        externalExpenseInvoiceValue +=
-          jobsiteMonthReport.expenseInvoices[i].value;
-      }
+  let externalExpenseInvoiceValue = 0,
+    internalExpenseInvoiceValue = 0,
+    accrualExpenseInvoiceValue = 0;
+  for (let i = 0; i < jobsiteMonthReport.expenseInvoices.length; i++) {
+    if (jobsiteMonthReport.expenseInvoices[i].accrual) {
+      accrualExpenseInvoiceValue += jobsiteMonthReport.expenseInvoices[i].value;
+    } else if (jobsiteMonthReport.expenseInvoices[i].internal) {
+      internalExpenseInvoiceValue +=
+        jobsiteMonthReport.expenseInvoices[i].value;
+    } else {
+      externalExpenseInvoiceValue +=
+        jobsiteMonthReport.expenseInvoices[i].value;
     }
-
-    let externalRevenueInvoiceValue = 0,
-      internalRevenueInvoiceValue = 0,
-      accrualRevenueInvoiceValue = 0;
-    for (let i = 0; i < jobsiteMonthReport.revenueInvoices.length; i++) {
-      if (jobsiteMonthReport.revenueInvoices[i].accrual) {
-        accrualRevenueInvoiceValue +=
-          jobsiteMonthReport.revenueInvoices[i].value;
-      } else if (jobsiteMonthReport.revenueInvoices[i].internal) {
-        internalRevenueInvoiceValue +=
-          jobsiteMonthReport.revenueInvoices[i].value;
-      } else {
-        externalRevenueInvoiceValue +=
-          jobsiteMonthReport.revenueInvoices[i].value;
-      }
-    }
-
-    const summary: RangeSummaryReportClass = {
-      externalExpenseInvoiceValue,
-      internalExpenseInvoiceValue,
-      accrualExpenseInvoiceValue,
-      externalRevenueInvoiceValue,
-      internalRevenueInvoiceValue,
-      accrualRevenueInvoiceValue,
-    };
-
-    jobsiteMonthReport.summary = summary;
-
-    return;
-  } catch (error) {
-    console.error("Error in summer", error);
   }
+
+  let externalRevenueInvoiceValue = 0,
+    internalRevenueInvoiceValue = 0,
+    accrualRevenueInvoiceValue = 0;
+  for (let i = 0; i < jobsiteMonthReport.revenueInvoices.length; i++) {
+    if (jobsiteMonthReport.revenueInvoices[i].accrual) {
+      accrualRevenueInvoiceValue += jobsiteMonthReport.revenueInvoices[i].value;
+    } else if (jobsiteMonthReport.revenueInvoices[i].internal) {
+      internalRevenueInvoiceValue +=
+        jobsiteMonthReport.revenueInvoices[i].value;
+    } else {
+      externalRevenueInvoiceValue +=
+        jobsiteMonthReport.revenueInvoices[i].value;
+    }
+  }
+
+  const summary: RangeSummaryReportClass = {
+    externalExpenseInvoiceValue,
+    internalExpenseInvoiceValue,
+    accrualExpenseInvoiceValue,
+    externalRevenueInvoiceValue,
+    internalRevenueInvoiceValue,
+    accrualRevenueInvoiceValue,
+  };
+
+  jobsiteMonthReport.summary = summary;
+
+  return;
 };
 
 const issues = async (jobsiteMonthReport: JobsiteMonthReportDocument) => {
-  try {
-    const dayReports = await jobsiteMonthReport.getDayReports();
+  const dayReports = await jobsiteMonthReport.getDayReports();
 
-    jobsiteMonthReport.issues = [
-      ...dayReportIssueGeneration(dayReports),
-      ...(await jobsiteReportIssueGenerator(
-        await jobsiteMonthReport.getJobsite(),
-        {
-          startTime: jobsiteMonthReport.startOfMonth,
-          endTime: dayjs(jobsiteMonthReport.startOfMonth)
-            .endOf("month")
-            .toDate(),
-        }
-      )),
-    ];
-  } catch (error) {
-    console.error("Error in issues", error);
-  }
+  jobsiteMonthReport.issues = [
+    ...dayReportIssueGeneration(dayReports),
+    ...(await jobsiteReportIssueGenerator(
+      await jobsiteMonthReport.getJobsite(),
+      {
+        startTime: jobsiteMonthReport.startOfMonth,
+        endTime: dayjs(jobsiteMonthReport.startOfMonth).endOf("month").toDate(),
+      }
+    )),
+  ];
 };
 
 const excel = async (jobsiteMonthReport: JobsiteMonthReportDocument) => {
-  try {
-    const workbook = await generateForRangeReport(jobsiteMonthReport);
+  const workbook = await generateForRangeReport(jobsiteMonthReport);
 
-    const buffer = await getWorkbookBuffer(workbook);
+  const buffer = await getWorkbookBuffer(workbook);
 
-    console.log("got here");
-
-    await uploadFile(
-      await jobsiteMonthReport.getExcelName(),
-      buffer,
-      SupportedMimeTypes.XLSX
-    );
-  } catch (error) {
-    console.error("Error in excel", error);
-  }
+  await uploadFile(
+    await jobsiteMonthReport.getExcelName(),
+    buffer,
+    SupportedMimeTypes.XLSX
+  );
 };
 
 export default {
--- server/src/models/JobsiteMonthReport/class/update.ts
+++ server/src/models/JobsiteMonthReport/class/update.ts
@@ -7,53 +7,48 @@ import { CrewTypes } from "@typescript/crew";
 import { UpdateStatus } from "@typescript/models";
 
 const document = async (jobsiteMonthReport: JobsiteMonthReportDocument) => {
-  try {
-    if (!jobsiteMonthReport.jobsite)
-      throw new Error("Jobsite month report does not have a jobsite");
+  if (!jobsiteMonthReport.jobsite)
+    throw new Error("Jobsite month report does not have a jobsite");
 
-    const dayReports = await JobsiteDayReport.getByJobsiteAndMonth(
-      jobsiteMonthReport.jobsite,
-      jobsiteMonthReport.startOfMonth
-    );
+  const dayReports = await JobsiteDayReport.getByJobsiteAndMonth(
+    jobsiteMonthReport.jobsite,
+    jobsiteMonthReport.startOfMonth
+  );
 
-    jobsiteMonthReport.dayReports = dayReports.map((report) => report._id);
+  jobsiteMonthReport.dayReports = dayReports.map((report) => report._id);
 
-    const crewTypes: CrewTypes[] = [];
-    for (let i = 0; i < dayReports.length; i++) {
-      const dayReport = dayReports[i];
+  const crewTypes: CrewTypes[] = [];
+  for (let i = 0; i < dayReports.length; i++) {
+    const dayReport = dayReports[i];
 
-      for (let j = 0; j < dayReport.crewTypes.length; j++) {
-        if (!crewTypes.includes(dayReport.crewTypes[j]))
-          crewTypes.push(dayReport.crewTypes[j]);
-      }
+    for (let j = 0; j < dayReport.crewTypes.length; j++) {
+      if (!crewTypes.includes(dayReport.crewTypes[j]))
+        crewTypes.push(dayReport.crewTypes[j]);
     }
-
-    jobsiteMonthReport.crewTypes = crewTypes;
-
-    await jobsiteMonthReport.generateExpenseInvoiceReports();
-    await jobsiteMonthReport.generateRevenueInvoiceReports();
-
-    await jobsiteMonthReport.generateSummary();
-
-    await jobsiteMonthReport.generateIssues();
-
-    jobsiteMonthReport.update.status = UpdateStatus.Updated;
-    jobsiteMonthReport.update.lastUpdatedAt = new Date();
-
-    await jobsiteMonthReport.save();
-
-    await JobsiteYearReport.requestBuild({
-      jobsiteId: jobsiteMonthReport.jobsite,
-      date: jobsiteMonthReport.startOfMonth,
-    });
-
-    await jobsiteMonthReport.generateExcel();
-
-    return;
-  } catch (error) {
-    console.error("Error in document update", error);
-    throw error;
   }
+
+  jobsiteMonthReport.crewTypes = crewTypes;
+
+  await jobsiteMonthReport.generateExpenseInvoiceReports();
+  await jobsiteMonthReport.generateRevenueInvoiceReports();
+
+  await jobsiteMonthReport.generateSummary();
+
+  await jobsiteMonthReport.generateIssues();
+
+  jobsiteMonthReport.update.status = UpdateStatus.Updated;
+  jobsiteMonthReport.update.lastUpdatedAt = new Date();
+
+  await jobsiteMonthReport.save();
+
+  await JobsiteYearReport.requestBuild({
+    jobsiteId: jobsiteMonthReport.jobsite,
+    date: jobsiteMonthReport.startOfMonth,
+  });
+
+  await jobsiteMonthReport.generateExcel();
+
+  return;
 };
 
 export default {
